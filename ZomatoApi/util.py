3933aec5cf789f1daf9b40e31521516180a8b274
alokkumarsbg <aalokkeshri@gmail.com>
2021-05-31 02:04:21 +0530

    Adding new function get_popularlocation_bycity
    New Function added. This function will pull all popular locations url

--- ZomatoApi/util.py
+++ ZomatoApi/util.py
@@ -2,6 +2,9 @@ import logging
 import requests
 from bs4 import BeautifulSoup
 import json
+import re
+
+headers={'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'}
 
 # Read the URL using request
 def get_restaurant_details(url):
@@ -11,7 +14,7 @@ def get_restaurant_details(url):
     """
     try:
         logging.info(f"Reading Restaurant: {url}")
-        headers={'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'}
+        
         response = requests.get(url,headers=headers)
         logging.info(f"Response code {response.status_code}")
         
@@ -29,3 +32,19 @@ def get_restaurant_details(url):
         
     except Exception as e:
         logging.info("Failed to Extract data")
+
+
+def get_popularlocation_bycity(cityurl):
+        """ Get details of popular locations of the city and its URL
+        This URL will further be used to extract the Restaurants
+        """
+        response = requests.get(cityurl,headers=headers)
+        print(f"Response code {response.status_code}")
+        
+        # Get the script value
+        soup = BeautifulSoup(response.content,'html.parser')
+        for txt in soup(text=re.compile(r'JSON.parse')):
+            json_str = json.loads(str(txt[51:-11]))#.strip())
+            rest_result=json.loads(json_str)
+        
+        return rest_result['pages']['city']['5']['sections']['SECTION_POPULAR_LOCATIONS']['locations']
